37c5b1aded35cf93712f639f34456b13428bc66e
Victor Trindade <victortrindade@gmail.com>
2022-08-17 17:30:09 -0300

    JWT + BcryptJS

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 
 // import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+
+import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "hello world!" });
-});
+routes.get("/", (req, res) => res.json({ message: "hello world!" }));
+
+// routes.post('/users', UserController.store);
+routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
+// routes.put("/users", UserController.update);
 
 export default routes;
